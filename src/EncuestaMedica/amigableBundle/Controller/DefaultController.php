3e5b0e22048e5fd44b5d6c2d3f3d3301748826b9
alejandrocostao <alejandro.costa@ene.es>
2014-10-21 15:41:06 +0200

    implementado routing
    routing ejercicio 2

---
+++ src/EncuestaMedica/amigableBundle/Controller/DefaultController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace EncuestaMedica\amigableBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+	var $medicos = array(
+	  array("codigo"=>1,"nombre"=>"Alejandro Costa"),
+	  array("codigo"=>2,"nombre"=>"Aurelio Moreno"),
+	  array("codigo"=>2,"nombre"=>"Alberto Gonzalez")
+	);
+    public function indexAction($name)
+    {
+        return $this->render('EncuestaMedicaamigableBundle:Default:index.html.twig', array('name' => $name));
+    }
+	    public function medicosAction()
+    {
+        return $this->render('EncuestaMedicaamigableBundle:Default:medicos.html.twig', array("medicos"=>$this->medicos));
+    }
+		public function medicoAction($codigo)
+    {
+        return $this->render('EncuestaMedicaamigableBundle:Default:medico.html.twig', array("medico"=>$this->medicos[$codigo-1]));
+    }
+}
---
+++ src/EncuestaMedica/amigableBundle/EncuestaMedicaamigableBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace EncuestaMedica\amigableBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EncuestaMedicaamigableBundle extends Bundle
+{
+}
